dbd23f5636b87b0a5b93e99ae74ed85a256c66bd
Jagwinder Singh <jagsingh@hotmail.co.nz>
2024-07-28 17:10:47 +1200

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
-  base: "./",
+  base: '/blackhorse-project/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Ensure this path is correct
+    },
+  },
 });
